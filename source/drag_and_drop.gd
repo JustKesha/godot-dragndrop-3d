fff9788fc9a9f6efb0c75d056f0bb2173e5dfcdc
kesha <73757143+JustKesha@users.noreply.github.com>
2025-02-03 22:51:24 +0400

    Added ability to drag non RigidBody objects using setpos

--- source/drag_and_drop.gd
+++ source/drag_and_drop.gd
@@ -1,14 +1,20 @@
 extends Node3D
 
-var DRAG_FORCE := 350.0
-var DRAG_RELATIVE_POSITION := Vector3.UP * .1
+var DRAG_SPEED := 8.0
 
-var ZOOM_SPEED := 0.15
-var ZOOM_MIN := 0.5
-var ZOOM_MAX := 1.65
+var USE_FORCE  := true
+var DRAG_FORCE := 320.0
 
-var STABILISATION_SPEED := 3.5
-var STABILISATION_ANGLE := Vector3(.2, 1, 0)
+var USE_ZOOM   := true
+var ZOOM_MIN   := 0.75
+var ZOOM_MAX   := 3.0
+var ZOOM_SPEED := 0.2
+
+var USE_STABILISATION   := true
+var STABILISATION_SPEED := 7.5
+var STABILISATION_ANGLE := Vector3.ONE * PI/6
+
+var DRAG_OFFSET := Vector3.ZERO
 
 const CONTROLS := {
 	# Values from Input map
@@ -19,10 +25,11 @@ const CONTROLS := {
 
 @onready var drag_raycast:RayCast3D = $"."
 
-# Currently only works for RigidBody3D
 var drag_object:Node3D
 var drag_distance:float
 var drag_angle:Vector3
+var drag_using_force:bool
+var drag_unfreeze_after:bool
 
 # HELPERS
 
@@ -33,7 +40,16 @@ func get_collision_distance(raycast:RayCast3D = drag_raycast) -> float:
 	return raycast.global_transform.origin.distance_to(raycast.get_collision_point())
 
 func is_object_draggable(object) -> bool:
+	return object is Node3D
+
+func is_object_forcable(object) -> bool:
 	return object is RigidBody3D
+
+func is_object_static(object) -> bool:
+	if not object is RigidBody3D:
+		return true
+	
+	return object.freeze
 
 func get_draggable_aimed(raycast:RayCast3D = drag_raycast) -> Node3D:
 	if not raycast:
@@ -48,7 +64,7 @@ func get_draggable_aimed(raycast:RayCast3D = drag_raycast) -> Node3D:
 
 func get_drag_position(
 		distance:float = drag_distance,
-		offset:Vector3 = Vector3.ZERO,
+		offset:Vector3 = DRAG_OFFSET,
 		raycast:RayCast3D = drag_raycast,
 	) -> Vector3:
 	if not raycast:
@@ -61,12 +77,14 @@ func get_drag_velocity(
 		delta:float,
 		force:float = DRAG_FORCE,
 		object:Node3D = drag_object,
+		distance:float = drag_distance,
+		offset:Vector3 = DRAG_OFFSET,
 		raycast:RayCast3D = drag_raycast,
 	) -> Vector3:
 	if not object or not raycast:
 		return Vector3.ZERO
 	
-	var drag_pos = get_drag_position()
+	var drag_pos = get_drag_position(distance, offset, raycast)
 	return (drag_pos - object.global_position) * delta * force
 
 # SETTERS
@@ -86,15 +104,29 @@ func set_drag_angle(angle:Vector3 = STABILISATION_ANGLE) -> Vector3:
 	drag_angle = angle
 	return drag_angle
 
+func set_using_force(
+		use_force:bool = USE_FORCE,
+		object:Node3D = drag_object
+	) -> bool:
+	var can_use_force = is_object_forcable(object)
+	
+	drag_using_force = can_use_force and use_force
+	
+	if object is RigidBody3D:
+		object.freeze = !drag_using_force
+		drag_unfreeze_after = !object.freeze
+	
+	return drag_using_force
+
 # ACTIONS
 
 func start_dragging(
 		object:Node3D = get_draggable_aimed(),
 		distance:float = get_collision_distance(),
 		angle:Vector3 = STABILISATION_ANGLE,
-		raycast:RayCast3D = drag_raycast,
+		use_force:bool = USE_FORCE,
 	) -> bool:
-	if not object or not raycast:
+	if not object:
 		return false
 	
 	if drag_object:
@@ -103,6 +135,7 @@ func start_dragging(
 	set_drag_object(object)
 	set_drag_distance(distance)
 	set_drag_angle(angle)
+	set_using_force(use_force)
 	
 	return true
 
@@ -110,21 +143,46 @@ func stop_dragging():
 	if not drag_object:
 		return
 	
-	# RigidyBody3D specific
-	drag_object.sleeping = false
+	if drag_object is RigidBody3D:
+		drag_object.sleeping = false
+		drag_object.freeze = !drag_unfreeze_after
 	
 	drag_object = null
 
-func drag(
+func drag_by_setpos(
 		delta:float,
-		force:float = DRAG_FORCE,
+		speed:float = DRAG_SPEED,
 		object:Node3D = drag_object,
 	):
 	if not object:
 		return
 	
+	object.global_position = object.global_position.lerp(
+		get_drag_position(), delta * speed
+	)
+
+func drag_by_force(
+		delta:float,
+		force:float = DRAG_FORCE,
+		object:RigidBody3D = drag_object
+	):
+	if not drag_object:
+		return
+	
 	object.linear_velocity = get_drag_velocity(delta, force, object)
 	object.angular_velocity = Vector3.ZERO
+
+func drag(
+		delta:float,
+		speed:float = DRAG_SPEED,
+		force:float = DRAG_FORCE,
+		use_force:bool = drag_using_force,
+		object:Node3D = drag_object,
+	):
+	if drag_using_force:
+		drag_by_force(delta, force, object)
+	else:
+		drag_by_setpos(delta, speed, object)
 
 func stabilize(
 		delta:float,
@@ -142,16 +200,17 @@ func stabilize(
 func _unhandled_input(event:InputEvent):
 	if event.is_action_pressed(CONTROLS.DRAG):
 		start_dragging()
-	
 	elif event.is_action_released(CONTROLS.DRAG):
 		stop_dragging()
 	
+	if not USE_ZOOM:
+		return
 	if event.is_action_pressed(CONTROLS.ZOOM_IN):
 		set_drag_distance(drag_distance - ZOOM_SPEED)
-	
 	elif event.is_action_pressed(CONTROLS.ZOOM_OUT):
 		set_drag_distance(drag_distance + ZOOM_SPEED)
 
 func _physics_process(delta:float):
 	drag(delta)
-	stabilize(delta)
+	if USE_STABILISATION:
+		stabilize(delta)
